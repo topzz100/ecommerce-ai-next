ce3897ffb24aaf671d4123b9e822b3e289bcc135
Tope Adenekan <adenekantope.com>
2026-03-09 10:11:35 +0100

    product details

---
+++ app/(app)/product/[slug]/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import ProductGallery from "@/components/app/ProductGallery";
+import ProductInfo from "@/components/app/ProductInfo";
+import { productList } from "@/lib/dummyData/productList";
+import { shuffle } from "@/lib/utils";
+import { notFound } from "next/navigation";
+
+interface ProductPageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { slug } = await params;
+
+  //   const { data: product } = await sanityFetch({
+  //     query: PRODUCT_BY_SLUG_QUERY,
+  //     params: { slug },
+  //   });
+  const product = shuffle(productList)[0];
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="min-h-screen bg-zinc-50 dark:bg-zinc-900">
+      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="grid gap-8 lg:grid-cols-2">
+          {/* Image Gallery */}
+          <ProductGallery images={product.images} productName={product.name} />
+
+          {/* Product Info */}
+          <ProductInfo product={product} />
+        </div>
+      </div>
+    </div>
+  );
+}
